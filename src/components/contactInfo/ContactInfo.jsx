7ca5c88ce668ddc59210391f7abae52fde34f36e
Valet-V0ult-de-Fur1e <82310399+Valet-V0ult-de-Fur1e@users.noreply.github.com>
2023-11-27 22:26:56 +0300

    fixed css bags

--- src/components/contactInfo/ContactInfo.jsx
+++ src/components/contactInfo/ContactInfo.jsx
@@ -27,7 +27,8 @@ const ContactInfo = () => {
               Контакты
             </h3>
             <h4>Телефон: <a href="tel:+7 (4212) 20-44-41">+7 (4212) 20-44-41</a></h4>
-            <h4>Почта: <a href="https://yandex.ru/maps/-/CDQ6M2kU">г.Хабаровск, проспект 60 лет Октября, д.156</a></h4>
+            <h4>Почта: <a href="mailto:27@ipsc.ru">27@ipsc.ru</a> </h4>
+            <h4>Адрес: <a href="https://yandex.ru/maps/-/CDQ6M2kU">г.Хабаровск, проспект 60 лет Октября, д.156</a></h4>
             <a href="https://catalog.ipsc.ru/listing/ro_habarovskiy/?v=f9308c5d0596">
               <Button>Узнать больше</Button>
             </a>
